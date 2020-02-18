bdb181171520de4aea1297fd57b41c1494b16255
TatsukiUchiyama <60599265+TatsukiUchiyama@users.noreply.github.com>
2020-02-18 17:20:47 +0900

    デプロイ準備

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
   resources :users
-  resources :groups do
+  resources :groups, do
     resources :messages
     namespace :api do
       resources :messages, only: :index, defaults: { format: 'json' }
